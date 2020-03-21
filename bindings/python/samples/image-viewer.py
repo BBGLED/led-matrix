2c8fd3dbad3c6675186eba1905b16785d1c8fa04
Bryan Ngo <bryanngo97@gmail.com>
2020-03-21 14:59:26 -0700

    add sample assets

--- bindings/python/samples/image-viewer.py
+++ bindings/python/samples/image-viewer.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python
+
+from samplebase import SampleBase
+
 import time
 import sys
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 
-if len(sys.argv) < 2:
-    sys.exit("Require an image argument")
-else:
-    image_file = sys.argv[1]
 
-image = Image.open(image_file)
+class RunText(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(RunText, self).__init__(*args, **kwargs)
 
-# Configuration for the matrix
-options = RGBMatrixOptions()
-options.rows = 32
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+    def run(self):
+        # Main function
+        if __name__ == "__main__":
+            run_text = RunText()
+            if (not run_text.process()):
+                run_text.print_help()
 
-matrix = RGBMatrix(options = options)
+        if len(sys.argv) < 2:
+            sys.exit("Require an image argument")
+        else:
+            image_file = sys.argv[1]
 
-# Make image fit our screen.
-image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+        image = Image.open(image_file)
 
-matrix.SetImage(image.convert('RGB'))
+        # Make image fit our screen.
+        image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
-try:
-    print("Press CTRL-C to stop.")
-    while True:
-        time.sleep(100)
-except KeyboardInterrupt:
-    sys.exit(0)
+        matrix.SetImage(image.convert('RGB'))
+
+        try:
+            print("Press CTRL-C to stop.")
+            while True:
+                time.sleep(100)
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+
+
